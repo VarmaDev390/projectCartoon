17348223e3806c7e757c68f882d9377d7458727e
VarmaDev390 <raviteja.letcetra@gmail.com>
2024-06-28 13:16:24 +0530

    server files

---
+++ server/server.js
@@ -0,0 +1,309 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+// import { chatBotAI } from "./src/routes/chatRoutes.js";
+import { config } from "dotenv";
+import OpenAI from "openai";
+import { callGPT } from "./src/components/helpers.js";
+import { v4 as uuidv4 } from "uuid";
+import * as mysql from "mysql";
+
+// setting the env variables
+const result = config();
+
+export const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const pool = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "project_cartoon",
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+    return;
+  }
+  console.log("Connected to the database");
+});
+
+const cartoonApp = express();
+const PORT = 3000;
+
+cartoonApp.use(bodyParser.json());
+cartoonApp.use(cors());
+// cartoonApp.use("/", chatBotAI);
+
+//endpoint for new chat
+cartoonApp.get("/chat", async (req, res) => {
+  // create a new sessionId
+  const sessionId = uuidv4();
+  // define the role and context for your chatbot
+  let messagesArr = [
+    {
+      role: "system",
+      content: "Act as an expert based on the knowledge of cartoon characters",
+    },
+  ];
+
+  let [collectedContent, role] = ["", ""];
+  try {
+    const stream = await callGPT(openai, messagesArr);
+    // after getting the data as stream we add all the chunks to a single string
+    for await (const chunk of stream) {
+      if (chunk.choices[0].delta.role) role = chunk.choices[0].delta.role;
+      if (chunk.choices[0]?.delta?.content) {
+        collectedContent += chunk.choices[0].delta.content;
+        res.write(chunk.choices[0].delta.content || "");
+        console.log(chunk.choices[0].delta);
+      }
+    }
+    // adding the collected chuncks and role as an object for saving it to db
+    messagesArr.push({
+      role: role,
+      content: collectedContent,
+    });
+
+    // store the chat history in the db
+    try {
+      const query =
+        "INSERT INTO chatData (sessionId, chatHistory) VALUES (?, ?)";
+      pool.query(query, [sessionId, messagesArr], (err, result) => {
+        if (err) {
+          console.error("Error inserting data:", err);
+        } else {
+          console.log("Data inserted successfully:", result);
+          res.end();
+        }
+      });
+    } catch (err) {
+      console.error("Error during query execution:", err);
+    }
+  } catch (err) {
+    console.error("Error during GPT stream processing:", err);
+  }
+  console.log("messagesArr", messagesArr);
+  res.end();
+});
+
+//endpoint for accessing old chats
+///chat/:sessionId/messages
+// cartoonApp.post("/chat/:sessionId", async (req, res) => {
+//   const sessionId = req.params.sessionId.slice(1);
+//   console.log("sessionId", sessionId);
+//   const message = req.body.message;
+//   console.log("message", message);
+
+//   let chatHistory = "";
+//   //get the chat history from db using sessionId
+
+//   // store the chat history in the db
+//   try {
+//     const query = `SELECT chatHistory FROM chatData WHERE sessionId = ?`;
+//     pool.query(query, [sessionId], async (err, result) => {
+//       if (err) {
+//         console.error("Error retreving data:", err);
+//       } else {
+//         console.log("Data retrevied successfully:", result[0].chatHistory);
+//         chatHistory = JSON.parse(result[0].chatHistory);
+//         if (chatHistory) {
+//           console.log("inide");
+//           let conversationObj = {
+//             role: "user",
+//             content: message,
+//           };
+//           chatHistory.push(conversationObj);
+//         }
+//         let [collectedContent, role] = ["", ""];
+
+//         const stream = await callGPT(openai, chatHistory);
+//         // after getting the data as stream we add all the chunks to a single string
+//         for await (const chunk of stream) {
+//           if (chunk.choices[0].delta.role) role = chunk.choices[0].delta.role;
+//           if (chunk.choices[0]?.delta?.content) {
+//             collectedContent += chunk.choices[0].delta.content;
+//             res.write(chunk.choices[0].delta.content || "");
+//             // console.log(chunk.choices[0].delta);
+//           }
+//         }
+//         chatHistory.push({
+//           role: role,
+//           content: collectedContent,
+//         });
+//         console.log("chatHistory", chatHistory);
+
+//         try {
+//           const updateQuery = `UPDATE chatData SET chatHistory = ? WHERE sessionId = ?`;
+//           pool.query(updateQuery, [JSON.stringify(chatHistory), sessionId]); // Ensure this is awaited
+//           console.log("Chat history updated successfully.");
+//         } catch (err) {
+//           console.error("Error updating chat history:", err);
+//         }
+//       }
+//       res.end();
+//     });
+//   } catch (err) {
+//     console.error("Error during query execution:", err);
+//   }
+// });
+
+//-------------------
+// cartoonApp.get("/", async (req, res) => {
+//   return res.json({ message: "Chat Application!" });
+// });
+
+// cartoonApp.post("/chat/:sessionId", async (req, res) => {
+//   const sessionId = req.params.sessionId.slice(1);
+//   console.log("sessionId", sessionId);
+//   const message = req.body.message;
+//   console.log("message", message);
+
+//   let chatHistory = [];
+
+//   try {
+//     const query = `SELECT chatHistory FROM chatData WHERE sessionId = ?`;
+//     const [rows] = await pool.query(query, [sessionId]);
+//     if (rows.length > 0) {
+//       console.log("Data retrieved successfully:", rows[0].chatHistory);
+//       chatHistory = JSON.parse(rows[0].chatHistory);
+//     } else {
+//       console.log("No chat history found for the given sessionId.");
+//     }
+//   } catch (err) {
+//     console.error("Error retrieving data:", err);
+//     return res.status(500).send("Internal server error");
+//   }
+
+//   if (chatHistory) {
+//     let conversationObj = {
+//       role: "user",
+//       content: message,
+//     };
+//     chatHistory.push(conversationObj);
+//   }
+
+//   let [collectedContent, role] = ["", ""];
+//   try {
+//     const stream = await callGPT(openai, chatHistory);
+//     for await (const chunk of stream) {
+//       if (chunk.choices[0].delta.role) role = chunk.choices[0].delta.role;
+//       if (chunk.choices[0]?.delta?.content) {
+//         collectedContent += chunk.choices[0].delta.content;
+//         res.write(chunk.choices[0].delta.content || "");
+//       }
+//     }
+//     chatHistory.push({
+//       role: role,
+//       content: collectedContent,
+//     });
+//     console.log("chatHistory", chatHistory);
+//   } catch (err) {
+//     console.error("Error calling GPT:", err);
+//     return res.status(500).send("Internal server error");
+//   }
+
+//   try {
+//     const updateQuery = `UPDATE chatData SET chatHistory = ? WHERE sessionId = ?`;
+//     await pool.query(updateQuery, [JSON.stringify(chatHistory), sessionId]);
+//     console.log("Chat history updated successfully.");
+//   } catch (err) {
+//     console.error("Error updating chat history:", err);
+//     return res.status(500).send("Internal server error");
+//   }
+
+//   res.end();
+// });
+
+// for single end point
+cartoonApp.post("/chat/:sessionId", async (req, res) => {
+  const sessionId = req.params.sessionId.slice(1);
+  console.log("sessionId", sessionId);
+  const message = req.body.message;
+  console.log("message", message);
+
+  let chatHistory = [];
+  let conversationObj = {};
+  //get the chat history from db using sessionId
+
+  // store the chat history in the db
+  try {
+    const query = `SELECT chatHistory FROM chatData WHERE sessionId = ?`;
+    pool.query(query, [sessionId], async (err, result) => {
+      if (err) {
+        console.error("Error retreving data:", err);
+        return;
+      }
+      console.log("result", result);
+
+      if (result.length > 0 && result[0].chatHistory) {
+        console.log("has history");
+
+        chatHistory = JSON.parse(result[0].chatHistory);
+        conversationObj = [
+          {
+            role: "user",
+            content: message,
+          },
+        ];
+      } else if (result.length === 0) {
+        console.log("no history");
+
+        conversationObj = [
+          {
+            role: "system",
+            content:
+              "Act as an expert based on the knowledge of cartoon characters",
+          },
+          {
+            role: "user",
+            content: message,
+          },
+        ];
+      }
+
+      chatHistory.push(...conversationObj);
+
+      let [collectedContent, role] = ["", ""];
+
+      const stream = await callGPT(openai, chatHistory);
+      // after getting the data as stream we add all the chunks to a single string
+      for await (const chunk of stream) {
+        if (chunk.choices[0].delta.role) role = chunk.choices[0].delta.role;
+        if (chunk.choices[0]?.delta?.content) {
+          collectedContent += chunk.choices[0].delta.content;
+          res.write(chunk.choices[0].delta.content || "");
+          // console.log(chunk.choices[0].delta);
+        }
+      }
+      chatHistory.push({
+        role: role,
+        content: collectedContent,
+      });
+      console.log("chatHistory", chatHistory);
+
+      const insertQuery = `INSERT INTO chatData (sessionId, chatHistory) VALUES (?, ?)`;
+      const updateQuery = `UPDATE chatData SET chatHistory = ? WHERE sessionId = ?`;
+      pool.query(
+        result.length === 0 ? insertQuery : updateQuery,
+        [sessionId, JSON.stringify(chatHistory)],
+        (err) => {
+          if (err) {
+            console.error("Error inserting chat history:", err);
+          } else {
+            console.log("Chat history created successfully.");
+          }
+        }
+      );
+
+      res.end();
+    });
+  } catch (err) {
+    console.error("Error during query execution:", err);
+  }
+});
+cartoonApp.listen(PORT, () =>
+  console.log("Example app is listening on port 3000.")
+);
